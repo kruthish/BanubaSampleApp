84cc2f231cf4a219988a315b06f8db0dc3200174
Vinay Vemshetti <Vinay.Vemshetti@ril.com>
2021-04-27 16:50:05 +0530

    Added bridging header

---
+++ BanubaSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BanubaSample-Bridging-Header.h
+//  BanubaSampleApp
+//
+//  Created by Vinay Vemshetti on 27/04/21.
+//
+
+#ifndef BanubaSample_Bridging_Header_h
+#define BanubaSample_Bridging_Header_h
+
+
+#endif /* BanubaSample_Bridging_Header_h */
+
+//#import "BNBOffscreenEffectPlayer.h"
